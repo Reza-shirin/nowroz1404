789fa17457aaf4d88c6bb7423f5b5c8fb0e55f07
Reza <87262854+Reza-shirin@users.noreply.github.com>
2025-04-03 10:11:52 +0330

    Update Program.cs

--- Soal8/Program.cs
+++ Soal8/Program.cs
@@ -1,1 +1,27 @@
-soal 8
+using System;
+
+namespace مضارب_5_بین_دو_عدد
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("لطفاً عدد اول را وارد کنید: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("لطفاً عدد دوم را وارد کنید: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("مضارب ۵ بین {0} و {1} عبارتند از:", a, b);
+            for (int i = a + 1; i < b; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
